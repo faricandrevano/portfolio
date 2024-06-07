536a100eaeff0c638f02e7c08788119e4ecccdba
faricandrevano <faric26andrevano@gmail.com>
2024-06-07 19:27:59 +0700

    modified data

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,39 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `With over 5 years of programming experience, I have developed a strong proficiency in a range of cutting-edge technologies, including Laravel, Flutter, React, Tailwind, MongoDB, and .NET. Throughout my career, I have successfully delivered high-quality, scalable, and efficient solutions. I have utilized Laravel to build powerful web applications and leveraged Flutter to create seamless cross-platform mobile apps.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `With over 5 years of programming experience, I have developed a strong proficiency in a range of cutting-edge technologies, including Laravel, Flutter, React, Tailwind, MongoDB, and .NET. Throughout my career, I have successfully delivered high-quality, scalable, and efficient solutions. I have utilized Laravel to build powerful web applications and leveraged Flutter to create seamless cross-platform mobile apps. My expertise in React has enabled me to develop dynamic user interfaces, while Tailwind CSS has helped me design visually appealing web interfaces. Additionally, my proficiency in MongoDB has allowed me to manage large datasets effectively, and my knowledge of .NET has facilitated the development of secure and versatile applications. My technical skills, combined with a continuous drive for learning and innovation, have been central to my success in the programming field.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    role: "Junior Programming Internship",
+    company: "Agung Logistics.",
+    description: `Led a team in developing and maintaining web applications using .NET, ASP.NET,and DEVEXPRESS. Implemented Tools Navicat and integrated with SQL SERVER databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: [".NET", "APS.NET", "DEVEXPRESS", "SQL SERVER"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "2023 - 2024",
+    role: "CHAIRMAN",
+    company: "SYNTAX COMMUNITY",
+    description: `Teaching programming to community members and managing the SYNTAX COMMUNITY website with Laravel, React and Tailwindcss technology.`,
+    technologies: ["LARAVEL", "REACTJS", "FLUTTER", "MYSQL"],
   },
   {
     year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+    role: "On The Job Training",
+    company: "INET MEDIA",
+    description: `Developed and maintained web applications using Laravel, Tailwindcss. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Laravel", "Tailwindcss"],
   },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
 ];
 
 export const PROJECTS = [
@@ -44,28 +44,28 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["HTML", "CSS", "React.js", "Node.js", "MongoDB"],
   },
   {
     title: "Task Management App",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+    technologies: ["HTML", "CSS", "React.js", "Firebase"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "React.js", "Tailwindcss"],
   },
   {
     title: "Blogging Platform",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    technologies: ["HTML", "CSS", "React.js", "Express", "mySQL"],
   },
 ];
 
