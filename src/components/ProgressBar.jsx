65bef519ffa8b3fe87f761338973132adeb5effa
faricandrevano <faric26andrevano@gmail.com>
2024-06-10 20:21:31 +0700

    modified progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,7 +2,9 @@ import {motion, useScroll} from 'framer-motion';
 function ProgressBar() {
     const {scrollYProgress} = useScroll();
   return (
-    <motion.div style={{scaleX: scrollYProgress}} className='fixed top-0 right-0 left-0 h-1 bg-purple-800 origin-left z-10'></motion.div>
+    <div className='fixed top-0 right-0 left-0 h-1 bg-neutral-600 z-10'>
+      <motion.div style={{scaleX: scrollYProgress}} className='fixed top-0 right-0 left-0 h-1 bg-purple-800 origin-left'></motion.div>
+    </div>
   )
 }
 
